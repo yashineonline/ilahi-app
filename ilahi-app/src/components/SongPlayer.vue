c9170f2fa576443dfe463b668d8389bec436814f
yashineonline <yashineonline@gmail.com>
2024-08-18 00:25:56 +0300

    updated TestViewBookView

---
+++ ilahi-app/src/components/SongPlayer.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container mx-auto p-4">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4">Song Lyrics</h1>
+        <div class="lyrics bg-gray-100 p-6 rounded-lg shadow-lg">
+          <p>{{ lyrics }}</p>
+        </div>
+        <div class="buttons mt-4 flex justify-center space-x-4">
+          <button class="btn btn-primary" @click="showTranslation">Show Translation</button>
+          <button class="btn btn-secondary" @click="openYoutubeLink">Watch on YouTube</button>
+          <button class="btn btn-accent" @click="generatePDF">Generate PDF</button>
+          <button class="btn btn-success" @click="downloadBook">Download PDF Book</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "SongPlayer",
+    data() {
+      return {
+        lyrics: "Your lyrics will appear here...",
+      };
+    },
+    methods: {
+      showTranslation() {
+        console.log("Show translation");
+      },
+      openYoutubeLink() {
+        console.log("Open YouTube link");
+      },
+      generatePDF() {
+        console.log("Generate PDF");
+      },
+      downloadBook() {
+        console.log("Download book");
+      },
+    },
+  });
+  </script>
+  
--- ilahi-app/src/router/index.ts
+++ ilahi-app/src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SongList from '../components/SongList.vue'
+import SongPlayer from '../components/SongPlayer.vue'
+import TestView from '../views/TestView.vue'
+import BookView from '../views/BookView.vue'
 
 const routes = [
   {
@@ -7,7 +11,26 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  // Add other routes here
+  {
+    path: '/test',
+    name: 'Test',
+    component: TestView
+  },
+  {
+    path: '/book',
+    name: 'Book',
+    component: BookView
+  },
+  {
+    path: '/songs',
+    name: 'SongList',
+    component: SongList
+  },
+  {
+    path: '/player/:id',
+    name: 'SongPlayer',
+    component: SongPlayer
+  }
 ]
 
 const router = createRouter({
